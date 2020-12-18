c24abb6952127d8c9b2c51539681c6a1147f33ba
Himanshuma38966 <74598469+Himanshuma38966@users.noreply.github.com>
2020-12-18 14:49:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+
+
+  
+var Engine = Matter.Engine;
+ World = Matter.World;
+ Events = Matter.Events
+ Bodies = Matter.Bodies;
+var particle;
+var plinkos = [];
+var divisions = [];
+var divisionHeight=300;
+var score =0;
+var gameState="play",points=0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+    for (var j = 75; j <=width; j=j+50) 
+    {
+       plinkos.push(new Plinko(j,75));
+    }
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+       plinkos.push(new Plinko(j,175));
+    }
+     for (var j = 75; j <=width; j=j+50) 
+    {
+       plinkos.push(new Plinko(j,275));
+    }
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+       plinkos.push(new Plinko(j,375));
+    }
+}
+function draw() {
+  background("cyan");
+  
+  
+  Engine.update(engine);
+  ground.display();
+  
+  for (var i = 0; i < plinkos.length; i++) {  
+  plinkos[i].display();  
+  }
+  if(particle!=null) 
+  {
+  particle.display();
+      
+  if (particle.body.position.y>660)
+  {
+  if (particle.body.position.x < 300) 
+  {
+  score=score+500;      
+  particle=null;
+ 
+ 
+  
+
+  }      
+  
+}
+
+   }
+   for (var i = 0; i < divisions.length; i++) {  
+    divisions[i].display();  
+  }
+  }
+  function mousePressed()
+{
+  if(gameState!=="end")
+  {
+      points++;
+     particle=new Particle(mouseX, 10, 10, 10); 
+  }   
+}
+
